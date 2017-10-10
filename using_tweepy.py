1ebb544023aaa9ec3b543c751022209174bfa3e4
coinlistener <coin.listener@gmail.com>
2017-10-10 07:59:24 -0400

    removing keys from tweepy

--- using_tweepy.py
+++ using_tweepy.py
@@ -1,10 +1,10 @@
 import tweepy
 
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'zlm1nd1niJfJuwPU1rhAeB6Bb'
-CONSUMER_SECRET = 'MR7yPaIA3YhLeZ93706pfvQC7P97I3iFfo0Ss2yTbD6UIo991F' 
-ACCESS_KEY = '904022760040660992-bpxV6iW1Cjj6c1xpmJoeJCpPvvQAvfQ'
-ACCESS_SECRET = 'qWQ134O0VFdp5QG8dGOBvVgPWo5eojPMisR1WRPfI7CJs'
+CONSUMER_KEY = 
+CONSUMER_SECRET = 
+ACCESS_KEY = 
+ACCESS_SECRET = 
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
